84215120814ce9f83c9f8b9fbd9674ce0ff9d28e
olmetadavid <olmeta.david@gmail.com>
2013-05-15 13:10:33 +0300

    Update dbug.php
    Clean the code with Drupal coding standard.

--- dbug.php
+++ dbug.php
@@ -2,6 +2,7 @@
 
 /**
  * Custom debug function : Usual and classic debugging function.
+ *
  * The two last parameters can be ignored.
  * If only one of them is set, the function will works.
  * Can set both additionnal parameters whatever their order.
@@ -32,10 +33,12 @@ function dbug() {
   $end = (is_bool($args[1])) ? $args[1] : $args[2];
   
   // Get method : check beofre if we are in backtrace method.
-  if (empty($args))
+  if (empty($args)) {
     $method = 5;
-  else
+  }
+  else {
     $method = (is_numeric($args[1])) ? $args[1] : $args[2];
+  }
   
   // Watchdog method : no die().
   if ($method == 4) {
@@ -43,31 +46,41 @@ function dbug() {
   }
   else {
 
-    // Select function to render $obj
+    // Select function to render $obj.
     switch ($method) {
       case 1:
         $handler = 'var_export';
         break;
+        
       case 2:
         $handler = 'var_dump';
         break;
+        
       default:
         $handler = 'print_r';
         break;
-    }//switch()
+        
+    }
     
-    // Output
+    // Output.
     print('<pre>');
-    if ($method == 3) // array_keys method
+    if ($method == 3) {
+      // array_keys method.
       $handler(array_keys((array)$obj));
-    elseif ($method == 5) // backtrace method
+    }
+    elseif ($method == 5) {
+      // backtrace method.
       $handler(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
-    else // other method
+    }
+    else {
+      // other method.
       $handler($obj);
+    }
     print('</pre>');
 
-    // Die... not Today ! (Force die for backtrace method)
-    if ((($end || is_null($end)) && $user == 1) || $method == 5)
+    // Die... not Today ! (Force die for backtrace method).
+    if ((($end || is_null($end)) && $user == 1) || $method == 5) {
       exit(0);
-  }//if()
+    }
+  }
 }
