d5a588b9d7da0006f00073ed384e8f5adfdaa4ec
Jonathan V <jvautier@clever-age.com>
2013-05-15 17:12:34 +0200

    Refactoring with reals method arguments

--- dbug.php
+++ dbug.php
@@ -1,86 +1,60 @@
 <?php
 
+define('DBUG_EXPORT', 'var_export');
+define('DBUG_DUMP', 'var_dump');
+define('DBUG_PRINT', 'print_r');
+define('DBUG_WATCHDOG', 'watchdog');
+define('DBUG_KEYS', 'array_keys');
+define('DBUG_BACKTRACE', 'debug_backtrace');
+define('DBUG_P_BACKTRACE', 'debug_print_backtrace');
+
 /**
  * Custom debug function : Usual and classic debugging function.
  *
- * The two last parameters can be ignored.
- * If only one of them is set, the function will works.
- * Can set both additionnal parameters whatever their order.
- * 
- * All methods :
- * dbug();        => print a backtrace, with die().
- * dbug($obj);    => print_r method (1)
- * dbug($obj, 1); => var_export method(1)
- * dbug($obj, 2); => var_dump method (1)
- * dbug($obj, 3); => print_r of array_keys : work with object or array (1)
- * dbug($obj, 4); => watchdog, without die().
- * 
- * (1) = (add a FALSE parameter to continue execution)
- * @global type $user
- * @param  Mixed  $obj Variable to debug
- * @param  int    $int  Type of debug
- * @param  Bool   $bool Set to TRUE to terminate function
+ * @param null $obj Variable to debug
+ * @param bool $end Set to TRUE to terminate function
+ * @param string $method Debug method
+ *
+ * @TODO Make Doc
+ *
  */
-function dbug() {
-  
-  global $user;
-  
-  // Get all parameters.
-  $args = func_get_args();
-  
-  // Get "object" to dbug, and the "end" arguments.
-  $obj = $args[0];
-  $end = (is_bool($args[1])) ? $args[1] : $args[2];
-  
-  // Get method : check beofre if we are in backtrace method.
-  if (empty($args)) {
-    $method = 5;
-  }
-  else {
-    $method = (is_numeric($args[1])) ? $args[1] : $args[2];
-  }
-  
-  // Watchdog method : no die().
-  if ($method == 4) {
-    watchdog('dbug_custom', serialize($obj));
-  }
-  else {
+function dbug ($obj = NULL, $end = TRUE, $method = DBUG_PRINT) {
 
-    // Select function to render $obj.
-    switch ($method) {
-      case 1:
-        $handler = 'var_export';
-        break;
-        
-      case 2:
-        $handler = 'var_dump';
-        break;
-        
-      default:
-        $handler = 'print_r';
-        break;
-        
-    }
-    
-    // Output.
-    print('<pre>');
-    if ($method == 3) {
-      // array_keys method.
-      $handler(array_keys((array)$obj));
-    }
-    elseif ($method == 5) {
-      // backtrace method.
-      $handler(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
+  if (!is_null($obj) || $obj == '') {
+
+    // Watchdog method : no die().
+    if ($method == DBUG_WATCHDOG) {
+      $method ('dbug_custom', serialize ($obj));
     }
     else {
-      // other method.
-      $handler($obj);
-    }
-    print('</pre>');
 
-    // Die... not Today ! (Force die for backtrace method).
-    if ((($end || is_null($end)) && $user == 1) || $method == 5) {
-      exit(0);
+      // Output.
+      print ('<pre>');
+      if ($method == DBUG_KEYS) {
+        // Ouput with array_keys method.
+        print_r ($method ((array) $obj));
+      }
+      else {
+        // Ouput with the giving method.
+        $method ($obj);
+      }
+      print('</pre>');
+
+      // Die... not Today !
+      if ($end || is_null($end)) {
+        exit (0);
+      }
     }
+  }
+  else {
+    // If NULL object and method isn't one of the backtrace method force method to debug_backtrace
+    if ($method != DBUG_BACKTRACE && $method != DBUG_P_BACKTRACE) {
+      $method = DBUG_BACKTRACE;
+    }
+    // To access to debug_print_backtrace method, call dbug(NULL, TRUE, DBUG_P_BACKTRACE);
+
+    // Output.
+    print_r ($method(DEBUG_BACKTRACE_IGNORE_ARGS));
+    exit (0);
   }
 }
